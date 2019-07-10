466709b2713282f63c9ad5c588051c28f1acd92e
wilda15 <52706324+wilda15@users.noreply.github.com>
2019-07-10 10:58:17 +0700

    https

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ],[
+            'addres.requaired' =>'wajib diisi',
         ]);
     }
 
@@ -65,6 +67,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'alamat' => $data['alamat'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- app/User.php
+++ app/User.php
@@ -15,9 +15,8 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    
+     protected $gurded = [];
 
     /**
      * The attributes that should be hidden for arrays.
